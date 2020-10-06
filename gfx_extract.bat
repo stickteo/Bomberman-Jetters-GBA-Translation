63d13d12d9ec9b087d56853c2b10bdf20b94e54f
unknown <stickteo@gmail.com>
2020-10-06 14:37:42 -0500

    use for loops in batch file

--- gfx_extract.bat
+++ gfx_extract.bat
@@ -1,12 +1,14 @@
-gbamdc -e rom\input.gba gfx_edit\equip_menu_0027388.bin 273388
+mkdir gfx_edit
+
+gbamdc -e rom\input.gba gfx_edit\equip_menu_00273388.bin 273388
 gbamdc -e rom\input.gba gfx_edit\battle_main_001b04d4.bin 1b04d4
 gbamdc -e rom\input.gba gfx_edit\battle_side_001b4488.bin 1b4488
 gbamdc -e rom\input.gba gfx_edit\start_menu_00245784.bin 245784
+gbamdc -e rom\input.gba gfx_edit\save_menu_00286ce8.bin 286ce8
 
-gba2bmp -t gfx_edit\equip_menu_0027388.bin -m gfx_map\equip_menu_00273388.map -b gfx_edit\equip_menu_0027388.bmp
-gba2bmp -t gfx_edit\battle_main_001b04d4.bin -m gfx_map\battle_main_001b04d4.map -b gfx_edit\battle_main_001b04d4.bmp
-gba2bmp -t gfx_edit\battle_side_001b4488.bin -m gfx_map\battle_side_001b4488.map -b gfx_edit\battle_side_001b4488.bmp
-gba2bmp -t gfx_edit\start_menu_00245784.bin -m gfx_map\start_menu_00245784.map -b gfx_edit\start_menu_00245784.bmp
+for %%G in (gfx_edit\*.bin) do gba2bmp -t gfx_edit\%%~nG.bin -m gfx_map\%%~nG.map -b gfx_edit\%%~nG.bmp
+
+mkdir gfx_eng
 
 robocopy /xc /xn /xo gfx_edit\ gfx_eng\ *.bmp
 
--- gfx_insert.bat
+++ gfx_insert.bat
@@ -1,10 +1,7 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
 
-gba2bmp -t gfx_edit\equip_menu_0027388.bin -m gfx_map\equip_menu_00273388.map -b gfx_eng\equip_menu_0027388.bmp -r
-gba2bmp -t gfx_edit\battle_main_001b04d4.bin -m gfx_map\battle_main_001b04d4.map -b gfx_eng\battle_main_001b04d4.bmp -r
-gba2bmp -t gfx_edit\battle_side_001b4488.bin -m gfx_map\battle_side_001b4488.map -b gfx_eng\battle_side_001b4488.bmp -r
-gba2bmp -t gfx_edit\start_menu_00245784.bin -m gfx_map\start_menu_00245784.map -b gfx_eng\start_menu_00245784.bmp -r
+for %%G in (gfx_edit\*.bin) do gba2bmp -t gfx_edit\%%~nG.bin -m gfx_map\%%~nG.map -b gfx_eng\%%~nG.bmp -r
 
 copy rom\input.gba rom\output_gfx.gba
 
@@ -12,5 +9,6 @@ gbamdc -cL7 gfx_edit\equip_menu_0027388.bin rom\output_gfx.gba 273388
 gbamdc -cL7 gfx_edit\battle_main_001b04d4.bin rom\output_gfx.gba 1b04d4
 gbamdc -cL7 gfx_edit\battle_side_001b4488.bin rom\output_gfx.gba 1b4488
 gbamdc -cL7 gfx_edit\start_menu_00245784.bin rom\output_gfx.gba 245784
+gbamdc -cL7 gfx_edit\save_menu_00286ce8.bin rom\output_gfx.gba 286ce8
 
 pause
