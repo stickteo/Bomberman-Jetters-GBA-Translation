3072623c6c9b2ea1e1f191b0af73d63d00358623
unknown <stickteo@gmail.com>
2020-10-06 22:55:49 -0500

    added more map files

--- gfx_extract.bat
+++ gfx_extract.bat
@@ -5,6 +5,10 @@ gbamdc -e rom\input.gba gfx_edit\battle_main_001b04d4.bin 1b04d4
 gbamdc -e rom\input.gba gfx_edit\battle_side_001b4488.bin 1b4488
 gbamdc -e rom\input.gba gfx_edit\start_menu_00245784.bin 245784
 gbamdc -e rom\input.gba gfx_edit\save_menu_00286ce8.bin 286ce8
+gbamdc -e rom\input.gba gfx_edit\gameover_00249214.bin 249214
+gbamdc -e rom\input.gba gfx_edit\kaiman_minigame_0019faa4.bin 19faa4
+gbamdc -e rom\input.gba gfx_edit\synthesis_001d8140.bin 1d8140
+gbamdc -e rom\input.gba gfx_edit\pause_story_00261184.bin 261184
 
 for %%G in (gfx_edit\*.bin) do gba2bmp -t gfx_edit\%%~nG.bin -m gfx_map\%%~nG.map -b gfx_edit\%%~nG.bmp
 
