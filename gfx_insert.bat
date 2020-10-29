881ddb965c023ce51061259bf19a3e5de59bc92d
unknown <stickteo@gmail.com>
2020-10-29 00:42:08 -0500

    translated title menu. logo still needs to be replaced

--- gfx_insert.bat
+++ gfx_insert.bat
@@ -15,11 +15,12 @@ rem gbamdc -cL7 gfx_edit\save_menu_00286ce8.bin rom\output_gfx.gba 286ce8
 rem gbamdc -cL7 gfx_edit\kaiman_minigame_0019faa4.bin rom\output_gfx.gba 19faa4
 rem gbamdc -cL7 gfx_edit\pause_story_00261184.bin rom\output_gfx.gba 261184
 
-for %%G in (gfx_edit\*.bin) do lzss -evn gfx_edit\%%~nG.bin
+for %%G in (gfx_edit\*.bin) do lzss -evo gfx_edit\%%~nG.bin
 
 insert rom\output_gfx.gba gfx_edit\synthesis_001d8140.bin 1d8140
 insert rom\output_gfx.gba gfx_edit\gameover_00249214.bin 249214
 insert rom\output_gfx.gba gfx_edit\equip_menu_00273388.bin 273388
+insert rom\output_gfx.gba gfx_edit\start_menu_00245784.bin 245784
 
 for %%G in (gfx_map_patch\*.ips) do flips -a gfx_map_patch\%%~nG.ips rom\output_gfx.gba
 
